7217a347452546df55ee53774612bfece56976d2
JunHyeong Lee <bljh5220@naver.com>
2026-06-05 14:36:57 +0900

    change provider.tf

--- providers.tf
+++ providers.tf
@@ -26,20 +26,26 @@ data "aws_eks_cluster" "this" {
   name = var.cluster_name
 }
 
-data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
-}
-
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--profile", "siseon"]
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.this.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.cluster_name, "--profile", "siseon"]
+    }
   }
 }
